6fac2622f9dae4b0f6ce788306c666b625af0067
Dheemanth-gowda <dheethu12@gmail.com>
2020-04-08 11:46:55 +0530

    added info and the current number of user

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 function enterSite() {
-    console.log("Hello");
     $("#landing").css("transform", "translate(-200vw)");
     $("#dimmed-bg").css("visibility", "visible");
     $("#dimmed-bg").css("transform", "translate(0)");
+    $("#above-fold").css("opacity", "1");
     $("#full-site").css("visibility", "visible");
-    $("#full-site").css("overflow", "auto");
-    $("#above-fold").css("opacity", "1")
+    $("#full-site").css("overflow", "visible");
+    $("#full-site").css("max-height", "auto");
+    $("#register-side-bar").css("transform", "translate(0)");
 }
+
+
+$("#visible-section").mouseenter(function() { openForm(); });
+$("#empty-section").mouseenter(function() { closeForm(); });
+$("#form-selection").mouseleave(function() { closeForm(); });
+
+function openForm() {
+    $("#register-side-bar").css("transform", "translate(-650px)");
+    $("#register-side-bar").css("transition-delay", "0s");
+    $("#register-side-bar").css("z-index", "100");
+
+    $("#call-num").css("opacity", "0");
+    $("#reg").css("opacity", "0");
+}
+
+function closeForm() {
+    $("#register-side-bar").css("transform", "translate(0)");
+    $("#register-side-bar").css("z-index", "0");
+    $("#call-num").css("opacity", "1");
+    $("#reg").css("opacity", "1");
+}
+
+
+var x = setInterval(function() {
+
+    let count = 2000;
+    count = count + 1;
+    now = now - count;
+    document.getElementById("userCount").innerHTML = now;
+}, 500)
